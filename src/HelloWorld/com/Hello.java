2608bd16742bee053dd3d3207127ae12af50177a
Cici-2006 <3280711378@qq.cn>
2025-02-19 15:25:53 +0800

    test

---
+++ src/HelloWorld/com/Hello.java
@@ -0,0 +1,14 @@
+package HelloWorld.com;
+
+import java.util.Arrays;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.print("Hello ");
+        System.out.print("World");
+        System.out.println("Hello World");
+    }
+}
